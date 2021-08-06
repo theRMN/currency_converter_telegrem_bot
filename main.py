047d805a4044c9c3566e9c2d75128319ad10e132
theRMN <roman.vassev@gmail.com>
2021-08-06 18:17:21 +0600

    MVP

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import logging
+from datetime import datetime
+
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
 from logic import actual_course
 from aiogram import Bot, Dispatcher, executor, types
 from config import API_TOKEN
@@ -7,7 +13,12 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+class Form(StatesGroup):
+    count = State()
 
 
 @dp.message_handler(commands=['start'])
@@ -26,25 +37,30 @@ async def menu(message: types.Message):
         reply=False)
 
 
-@dp.message_handler(commands=['ac'])
-async def ac(message: types.Message):
-    await message.reply(f'Актуальный курс:\n1 доллар = {actual_course()} тенге', reply=False)
-
-
-@dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def converter(message: types.Message):
+@dp.message_handler(state=Form.count)
+async def converter(message: types.Message, state: FSMContext):
     course = float(actual_course())
-    text = message.text
+    count = message.text
     try:
-        text = int(text)
-        await message.reply(f'{text} долларов = {text * round(course, 1)} тенге', reply=False)
+        count = int(count)
+        await message.reply(f'{count} долларов = {count * round(course, 1)} тенге', reply=False)
+        await state.finish()
     except ValueError:
         await message.reply('Введите число:')
 
 
-# @dp.message_handler(commands=['calculate'])
-# async def cc(message: types.Message):
-#     ...
+@dp.message_handler(commands=['ac'])
+async def ac(message: types.Message):
+    await message.reply(f'Актуальный курс на момент {datetime.now(tz=None)}:'
+                        f'\n1 доллар = {actual_course()} тенге', reply=False)
+
+
+@dp.message_handler(commands=['calculate'])
+async def calc(message: types.Message):
+    await Form.count.set()
+    await message.reply("Введите число", reply=False)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
+
